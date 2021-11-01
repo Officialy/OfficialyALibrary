8f78cec83b86e5fa15f2d762bb1211f5f10fb419
Max <mcmad.mm@gmail.com>
2021-11-01 13:57:57 +0000

    e

--- src/main/java/mods/officialy/officialyanapi/OfficialyALibrary.java
+++ src/main/java/mods/officialy/officialyanapi/OfficialyALibrary.java
@@ -1,16 +1,10 @@
 package mods.officialy.officialyanapi;
 
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod(OfficialyALibrary.MODID)
 @Mod.EventBusSubscriber(modid = OfficialyALibrary.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -18,21 +12,10 @@ public class OfficialyALibrary {
 
     public static final String MODID = "officialyalibrary";
 
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-    public static final DeferredRegister<Fluid> FLUIDS = DeferredRegister.create(ForgeRegistries.FLUIDS, MODID);
-    public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, MODID);
 
     public OfficialyALibrary(){
         final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        init(modEventBus);
-    }
-
-    public static void init(final IEventBus modEventBus) {
-        BLOCKS.register(modEventBus);
-        BLOCK_ENTITIES.register(modEventBus);
-        ITEMS.register(modEventBus);
     }
 
     @SubscribeEvent
--- src/main/java/mods/officialy/officialyanapi/block/entity/BaseBlockEntity.java
+++ src/main/java/mods/officialy/officialyanapi/block/entity/BaseBlockEntity.java
@@ -1,12 +1,14 @@
 package mods.officialy.officialyanapi.block.entity;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class BaseBlockEntity extends BlockEntity {
 
-    public BaseBlockEntity(BlockEntityType<?> blockEntityType) {
-        super(blockEntityType, null, null);
+    public BaseBlockEntity(BlockEntityType<?> blockEntityType, BlockPos pos, BlockState state) {
+        super(blockEntityType, pos, state);
     }
 
 }
