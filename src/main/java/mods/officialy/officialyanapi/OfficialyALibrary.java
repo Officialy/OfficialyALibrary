670c257e565031a733f6340c015f7537c6ae028a
Max <mcmad.mm@gmail.com>
2021-11-01 13:49:50 +0000

    e

--- src/main/java/mods/officialy/officialyanapi/OfficialyALibrary.java
+++ src/main/java/mods/officialy/officialyanapi/OfficialyALibrary.java
@@ -4,22 +4,39 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.material.Fluid;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+@Mod(OfficialyALibrary.MODID)
+@Mod.EventBusSubscriber(modid = OfficialyALibrary.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class OfficialyALibrary {
 
-    public static final String MOD_ID = "kontext";
+    public static final String MODID = "officialyalibrary";
 
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MOD_ID);
-    public static final DeferredRegister<Fluid> FLUIDS = DeferredRegister.create(ForgeRegistries.FLUIDS, MOD_ID);
-    public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, MOD_ID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    public static final DeferredRegister<Fluid> FLUIDS = DeferredRegister.create(ForgeRegistries.FLUIDS, MODID);
+    public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, MODID);
 
-    public static void init() {
-        BLOCKS.register();
-        BLOCK_ENTITIES.register();
-        ITEMS.register();
+    public OfficialyALibrary(){
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        init(modEventBus);
     }
 
+    public static void init(final IEventBus modEventBus) {
+        BLOCKS.register(modEventBus);
+        BLOCK_ENTITIES.register(modEventBus);
+        ITEMS.register(modEventBus);
+    }
+
+    @SubscribeEvent
+    public static void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
 }
